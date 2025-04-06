0654aad66f5828fe1a12a23e9f4af84a208a7e3b
Daniel <daniel@datascientest.com>
2025-04-06 21:29:50 +0200

    ajout feature_question_sql_collegue

--- app.py
+++ app.py
@@ -1,40 +1,68 @@
-import streamlit as st
 import pandas as pd
 import duckdb
-
-st.write("""
-# SQL SRS
-Space Repetition System SQL practice
-""")
+import streamlit as st
+import io
 
 
-option = st.selectbox(
-   "What would you like to review?",
-   ("Joins", "GroupBy", "Windows Functions"),
-   index=None,
-   placeholder="Select a theme...",
-)
+# 1er df "beverages"
+csv = '''
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+'''
+
+beverages = pd.read_csv(io.StringIO(csv))
 
 
-st.write('You selected:', option)
+# 2eme df "food_items"
+csv2 = '''
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+'''
+
+food_items = pd.read_csv(io.StringIO(csv2))
 
 
-data = {"a": [1, 2, 3], "b": [4, 5, 6]}
-df = pd.DataFrame(data)
+# réponse (écrite en tte lettre = string) que l'utilisateur doit donner pour que ça fonctionne :
+answer = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+# voici la df de résultat
+# la solution c la df de résultat qd on prend la query "answer" et kon la met dans duckdb.sql
+solution = duckdb.sql(answer).df()
 
-with tab1:
-    sql_query = st.text_area(label="entrez votre input")
-    result = duckdb.query(sql_query).df()
-    st.write(f"Vous avez entré la query suivante: {sql_query}")
+# on met à dispo de l'utilisateur, un text area pour que l'utilisateur puisse entrer sa requete SQL :
+st.header("enter your code")
+query = st.text_area(label="votre code SQL ici", key="user_input") # cette query sert à montrer la df de résultat
+if query:
+    result = duckdb.sql(query).df()
     st.dataframe(result)
 
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+
+
+# ces tables presentent les tables qu'a l'utilisateur à sa disposition, et on lui montre
+# aussi la solution cad la df qui est attendu en sortie
+
+# dans la tab2, on montre à l'utilisateur, le résultat attendu (solution) et les tables ki sont
+# à sa disposotion (beverages & food_items)
+with tab2:
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected:")
+    st.dataframe(solution)
+
+# et dans une autre table à part (cela permet de cacher le fait d'avoir des tables à part),
+# il uy aura la réponse (s'il ne trouve pas il peut cliquer sur la table pour voir la réponse)
 with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.write(answer)
 
